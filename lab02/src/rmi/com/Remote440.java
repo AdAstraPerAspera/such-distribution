5da48fce7fe99f1bdd12009e53ab1b5622426a2a
AdAstraPerAspera <mw1000us@gmail.com>
2013-10-05 20:49:29 -0400

    Removed unnecessary code.
    Fixing old incorrect stuff.

--- lab02/src/rmi/com/Remote440.java
+++ lab02/src/rmi/com/Remote440.java
@@ -7,14 +7,5 @@ package rmi.com;
  * @author Michael Wang - mhw1
  */
 public interface Remote440 {
-	
-	/**
-	 * Processes an RMIMessage object and invokes the function referenced
-	 * therein.  Throws an exception if the function ID is invalid.
-	 * @param message - the message invoking the method
-	 * @return RMIMessage containing the return value and arguments to the
-	 * function
-	 */
-	public RMIResponseMessage remoteCall(RMIInvocationMessage message) throws Exception;
-	
+		
 }
