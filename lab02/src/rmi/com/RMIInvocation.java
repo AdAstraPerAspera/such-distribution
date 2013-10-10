8cb4383487fb369422d9edf2a1def36fb254cb71
Cyphrus <wmaynes@cmu-875306.wv.cc.cmu.edu>
2013-10-09 23:06:32 -0400

    Started Client Side Invocation

---
+++ lab02/src/rmi/com/RMIInvocation.java
@@ -0,0 +1,14 @@
+package rmi.com;
+
+import java.io.Serializable;
+import rmi.reg.ReferenceObject;
+
+public class RMIInvocation {
+	
+	public static Object invoke (String fnId, String name, ReferenceObject ror, Serializable... args) {
+		
+		
+		return ror;
+	}
+	
+}
