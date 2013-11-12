fb739004d22727999de106bfc7456ed7d35e33a8
Cyphrus <wmaynes@wmaynesms.andrew.cmu.edu>
2013-11-12 12:33:29 -0500

    DFS stuff

--- lab03/src/mrf/dfs/DFSCoordinator.java
+++ lab03/src/mrf/dfs/DFSCoordinator.java
@@ -1,5 +1,63 @@
 package mrf.dfs;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import mrf.config.ConfigParser;
+
 public class DFSCoordinator {
 
+	HashMap<String, HashSet<String>> part2file;
+	HashMap<String, HashSet<String>> file2part;
+	HashMap<String, String> partLoc;
+	int repfactor;
+	
+	
+	/*
+	 * TODO: Parse config
+	 * 
+	 * TODO: Send config to participants
+	 * 
+	 * TODO: Replicate files among participants
+	 * TODO: Track Replication
+	 * TODO: Mark participants as active of down
+	 * 
+	 * TODO: Send a copy of a file from one node to another if requested
+	 * 
+	 * TODO: Threads
+	 */
+	
+	
+	public DFSCoordinator (String configPath, String initData) throws Exception {
+		try {
+			FileInputStream fis = new FileInputStream(configPath);
+			HashMap<String, String> parsed = ConfigParser.parse(fis);
+			fis.close();
+			
+			for(String s: parsed.keySet()) {
+				if(s.equals("factor")) {
+					this.repfactor = Integer.parseInt(parsed.get("factor"));
+				}
+				else {
+					HashSet<String> files = new HashSet();
+					this.part2file.put(s, files);
+					this.partLoc.put(s, parsed.get(s));
+				}
+			}
+			if (this.repfactor <= 0) {
+				throw new Exception("Failed to parse a valid replication factor from the config file");
+			}
+			
+			
+
+			
+		} catch (FileNotFoundException e) {
+			System.err.println("Invalid Config File Path: " + e);
+		} catch (IOException e) {
+			System.err.println("IOException while parsing config: " + e);
+		}
+	}
 }
--- lab03/src/mrf/dfs/DFSParticipant.java
+++ lab03/src/mrf/dfs/DFSParticipant.java
@@ -2,4 +2,15 @@ package mrf.dfs;
 
 public class DFSParticipant {
 
+	/*
+	 * TODO: Write File to local-space AND send to coordinator for replication
+	 * 
+	 * TODO: Read file from local-space
+	 * 
+	 * TODO: Get an ORIGINAL local copy from the coordinator
+	 * 
+	 * TODO: Return a TEMP copy to the coordinator
+	 * 
+	 * TODO: Return list of ORIGINAL replicas that it has access to.
+	 */
 }
