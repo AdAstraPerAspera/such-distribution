13cd2c41913cb0d5fb896b1820383614adf0ff68
AdAstraPerAspera <mw1000us@gmail.com>
2013-11-14 22:37:47 -0500

    Changes to ImageMap/ImageReduce

--- lab03/src/mrf/example/ImageMap.java
+++ lab03/src/mrf/example/ImageMap.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 
 import mrf.tasks.AbstractMapCallable;
 
-public class ImageMap extends AbstractMapCallable<ArrayList<Byte>, ArrayList<Byte>> {
+public class ImageMap extends AbstractMapCallable<ArrayList<Integer>, ArrayList<Integer>> {
 
 	@Override
-	public ArrayList<Byte> map(ArrayList<Byte> input) {
-		ArrayList<Byte> res = new ArrayList<Byte>();
+	public ArrayList<Integer> map(ArrayList<Integer> input) {
+		ArrayList<Integer> res = new ArrayList<Integer>();
 		for(int i = 0; i < input.size(); i++) {
-			res.add((byte) (input.get(i) ^ 0xff));
+			res.add((int) (input.get(i) ^ 0x00ffffff));
 		}
 		return res;
 	}
--- lab03/src/mrf/example/ImageReduce.java
+++ lab03/src/mrf/example/ImageReduce.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 
 import mrf.tasks.ReduceCallable;
 
-public class ImageReduce implements ReduceCallable<ArrayList<Byte>> {
+public class ImageReduce implements ReduceCallable<ArrayList<Integer>> {
 
 	@Override
-	public ArrayList<Byte> reduce(ArrayList<ArrayList<Byte>> f) {
-		ArrayList<Byte> res = new ArrayList<Byte>();
+	public ArrayList<Integer> reduce(ArrayList<ArrayList<Integer>> f) {
+		ArrayList<Integer> res = new ArrayList<Integer>();
 		for(int i = 0; i < f.size(); i++){
 			res.addAll(f.get(i));
 		}
