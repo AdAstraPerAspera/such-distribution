d0be7f2cf554c48b02b17be6edd7feca8d61158c
AdAstraPerAspera <mw1000us@gmail.com>
2013-12-07 10:48:35 -0500

    changes to parallel version

--- lab04/src/kmeans/main/MainClass.java
+++ lab04/src/kmeans/main/MainClass.java
@@ -20,16 +20,10 @@ class MainClass {
 		if(myrank == 0){
 			//I am master.
 			Master.runMaster(args, size);
-			
-			System.out.println("This is the master, rank " + myrank);
 		} else {
 			//I am slave.
 			Slave.runSlave();
-			
-			System.out.println("This is a slave, rank " + myrank);
 		}
-		
-		System.out.println("Total size of system is: " + size);
 		
 		MPI.Finalize();
 	}
