29cdd03f6b71acee62bb0b186073257707431cfa
AdAstraPerAspera <mw1000us@gmail.com>
2013-12-06 20:17:38 -0500

    request/response structure for cartesian points

--- lab04/src/kmeans/main/Master.java
+++ lab04/src/kmeans/main/Master.java
@@ -6,7 +6,9 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import kmeans.func.Calcs;
 import kmeans.type.DataType;
+import kmeans.type.Group;
 import kmeans.type.Point;
 import kmeans.type.ReqObj;
 
@@ -27,6 +29,7 @@ public class Master {
 		int 				length	  = 0;
 		int                 clusters  = 0;
 		double              eps       = 0.0;
+		double              change    = Double.MAX_VALUE;
 		ArrayList<String>   dnaData   = new ArrayList<String>();
 		ArrayList<Point>    pointData = new ArrayList<Point>();
 		
@@ -102,11 +105,90 @@ public class Master {
 					chunks.add(new ArrayList<String>());
 				}
 				
-				for(int i = 0; i < dnaData.size(); i ++){
+				for(int i = 0; i < dnaData.size(); i++){
 					chunks.get(i % size).add(dnaData.get(i));
+				}
+				
+				ArrayList<String> means = new ArrayList<String>();
+				int partSize = dnaData.size()/K;
+				for(int i = 0; i < partSize; i++){
+					String newMean = dnaData.get((int)(Math.random() * partSize));
+					while(means.contains(newMean)) newMean = dnaData.get((int)(Math.random() * partSize));
+					means.add(newMean);
+				}
+				
+				while(change > eps){
+					
+					
+					
+					
+					/*ArrayList<Group<String>> groupedDNA = assocDNA(dnaData, means);
+					ArrayList<String> newMeans = recalculateDNA(means, groupedDNA);
+					double maxChange = 0.0;
+					for(int i = 0; i < newMeans.size(); i++) {
+						double change = (Calcs.dnaDistance(means.get(i), newMeans.get(i)) / (n * 1.0));
+						if (change > maxChange) { maxChange = change; }
+					}
+					means = newMeans;
+					maxProportionChange = maxChange;*/
 				}
 			} else {
 				//work with points
+				
+				//split initial data into chunks to be processed
+				ArrayList<ArrayList<Point>> chunks = new ArrayList<ArrayList<Point>>();
+				for(int i = 0; i < size - 1; i++){
+					chunks.add(new ArrayList<Point>());
+				}
+				
+				for(int i = 0; i < pointData.size(); i ++){
+					chunks.get(i % (size - 1)).add(pointData.get(i));
+				}
+				
+				ArrayList<Point> means = new ArrayList<Point>();
+				int partSize = pointData.size() / clusters;
+				for(int i = 0; i < partSize; i++){
+					Point newMean = pointData.get((int)(Math.random() * partSize));
+					while(means.contains(newMean)){ newMean = pointData.get((int)(Math.random() * partSize)); }
+					means.add(newMean);
+				}
+				
+				while(change > eps){
+					Request[] reqs = new Request[size];
+					Object[][] resps = new Object[size][1];
+					
+					for(int i = 1; i < size; i++){
+						ReqObj message = new ReqObj(ReqObj.ReqType.ASSOC, DataType.POINT, means, chunks.get(i));
+						
+						MPI.COMM_WORLD.Isend(message, 0, 1, MPI.OBJECT, i, size);
+						
+						reqs[i] = MPI.COMM_WORLD.Irecv(resps[i], 0, 1, MPI.OBJECT, i, MPI.ANY_TAG);
+					}
+					
+					MPI.COMM_WORLD.Waitall(reqs);
+					
+					for(int i = 1; i < size; i++){
+						//process responses
+					}
+					
+					for(int i = 1; i < clusters; i++){
+						//send new requests
+					}
+					
+					for(int i = 1; i < clusters; i++){
+						//process responses
+					}
+					
+					/*ArrayList<Group<Point>> groupedPoints = assocPoints(pData, means);
+					ArrayList<Point> newMeans = recalculateMeans(means, groupedPoints);
+					double maxChange = 0.0;
+					for(int i = 0; i < newMeans.size(); i++) {
+						double change = Calcs.cartesianDistance(means.get(i), newMeans.get(i));
+						if (change > maxChange) { maxChange = change; }
+					}
+					means = newMeans;
+					maxMeanChange = maxChange;*/
+				}
 			}
 		}
 	}
